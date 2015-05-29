5d829afb2eaa60d39d5e8820fc5bc564f23806c8
motorolja <rasmusholm957@hotmail.com>
2015-05-29 18:09:31 +0200

    added button for togling map and a non functional GeoMap

---
+++ networkmonitor/src/main/java/ca/rmen/android/networkmonitor/app/map/GeoMapActivity.java
@@ -0,0 +1,120 @@
+/*
+ * This source is part of the
+ *      _____  ___   ____
+ *  __ / / _ \/ _ | / __/___  _______ _
+ * / // / , _/ __ |/ _/_/ _ \/ __/ _ `/
+ * \___/_/|_/_/ |_/_/ (_)___/_/  \_, /
+ *                              /___/
+ * repository.
+ *
+ * Copyright (C) 2015 Rasmus Holm
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ca.rmen.android.networkmonitor.app.map;
+
+import java.io.File;
+
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+import org.mapsforge.map.android.util.AndroidUtil;
+import org.mapsforge.map.android.view.MapView;
+import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.renderer.TileRendererLayer;
+import org.mapsforge.map.reader.MapDataStore;
+import org.mapsforge.map.reader.MapFile;
+import org.mapsforge.map.rendertheme.InternalRenderTheme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+
+import ca.rmen.android.networkmonitor.Constants;
+import ca.rmen.android.networkmonitor.R;
+
+/*
+ * Displays logged data in a map that is based on "OpenStreetMap"
+ * The API used is "mapsforge" https://github.com/mapsforge/mapsforge
+ * "mapsforge" is an open source api for handling offline maps from "OpenStreetMap" on Android
+ *
+ * The reason for not using google maps is that it is not open source
+ */
+
+public class GeoMapActivity extends Activity {
+    private static final String TAG = Constants.TAG + GeoMapActivity.class.getSimpleName();
+
+    // name of the map file in the external storage
+    private static final String MAPFILE = "sweden.map";
+
+    private MapView mapView;
+    private TileCache tileCache;
+    private TileRendererLayer tileRendererLayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.mapView = new MapView(this);
+        // setContentView(this.mapView);
+
+        this.mapView.setClickable(true);
+        this.mapView.getMapScaleBar().setVisible(true);
+        this.mapView.setBuiltInZoomControls(true);
+        this.mapView.getMapZoomControls().setZoomLevelMin((byte) 10);
+        this.mapView.getMapZoomControls().setZoomLevelMax((byte) 20);
+
+        // create a tile cache of suitable size
+        this.tileCache = AndroidUtil.createTileCache(this, "mapcache",
+                mapView.getModel().displayModel.getTileSize(), 1f,
+                this.mapView.getModel().frameBufferModel.getOverdrawFactor());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // (58.3991, 15.5772) is the coordinates for LIU (Linköpings University) in Sweden
+        this.mapView.getModel().mapViewPosition.setCenter(new LatLong(58.3991, 15.5772));
+        this.mapView.getModel().mapViewPosition.setZoomLevel((byte) 12);
+
+        // tile renderer layer using internal render theme
+        MapDataStore mapDataStore = new MapFile(getMapFile());
+        this.tileRendererLayer = new TileRendererLayer(tileCache, mapDataStore,
+                this.mapView.getModel().mapViewPosition, false, true, AndroidGraphicFactory.INSTANCE);
+        tileRendererLayer.setXmlRenderTheme(InternalRenderTheme.OSMARENDER);
+
+        // only once a layer is associated with a mapView the rendering starts
+        this.mapView.getLayerManager().getLayers().add(tileRendererLayer);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        this.mapView.getLayerManager().getLayers().remove(this.tileRendererLayer);
+        this.tileRendererLayer.onDestroy();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.tileCache.destroy();
+        this.mapView.getModel().mapViewPosition.destroy();
+        this.mapView.destroy();
+        AndroidGraphicFactory.clearResourceMemoryCache();
+    }
+
+    private File getMapFile() {
+        File file = new File(Environment.getExternalStorageDirectory(), MAPFILE);
+        return file;
+    }
+}
